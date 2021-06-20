942b61ae6c39e3ac77e7c495586eda2a7a9ba2c9
살려주세요 <46598063+qwreey75@users.noreply.github.com>
2021-06-20 22:16:57 +0900

    Update init.lua

--- UIRender/init.lua
+++ UIRender/init.lua
@@ -2,89 +2,103 @@ local renderClass = {};
 renderClass.__index = {};
 
 function renderClass.new(settings)
-  settings = settings or {};
-  local this = {};
-  this.storage = settings.storage or {};
-  this.uiHolder = settings.uiHolder;
-  this.class = settings.class or {};
+    if settings == renderClass then
+        error("do not call new function with ':'! using '.' for fix this error (or using this on inited class?)");
+    end
+    settings = settings or {};
+    local this = {};
+    this.storage = settings.storage or {};
+    this.uiHolder = settings.uiHolder;
+    this.class = settings.class or {};
 
-  setmetatable(this,renderClass);
+    setmetatable(this,renderClass);
 end
 
-local function newObject(ClassName,prop)
-	local class = self.class[ClassName];
-  local new = class and class() or Instance.new(ClassName);
-	for index,value in pairs(prop) do
-		local valueType = typeof(value);
-		local indexType = typeof(index);
+function renderClass:newObject(ClassName,prop)
+    if renderClass == self or (not self) then
+        error("you can't use this function on none inited class, using className.new() for using this function");
+    end
+    local class = self.class[ClassName];
+    local new = class and class() or Instance.new(ClassName);
+    for index,value in pairs(prop) do
+        local valueType = typeof(value);
+        local indexType = typeof(index);
 
-		-- child
-		if indexType ~= "string" and valueType == "Instance" then
-			value.Parent = new;
-		elseif valueType == "function" then
-			-- connect event
-			if index ~= "whenCreated" then
-				new[index]:Connect(value);
-			end
-		elseif indexType == "string" then
-			-- property
-			new[index] = value
-		end
-	end
-	local whenCreated = prop["whenCreated"];
-	if whenCreated then
-		whenCreated(new);
-	end
-	return new;
-end
-
-function renderClass:newObject(...)
-  newObject(...);
+        -- child
+        if indexType ~= "string" and valueType == "Instance" then
+            value.Parent = new;
+        elseif valueType == "function" then
+            -- connect event
+            if index ~= "whenCreated" then
+                new[index]:Connect(value);
+            end
+        elseif indexType == "string" then
+            -- property
+            new[index] = value
+        end
+    end
+    local whenCreated = prop["whenCreated"];
+    if whenCreated then
+        whenCreated(new);
+    end
+    return new;
 end
 
 function renderClass:getObject(name)
-	return self.storage[name];
+    if renderClass == self or (not self) then
+        error("you can't use this function on none inited class, using className.new() for using this function");
+    end
+    return self.storage[name];
 end
 
 function renderClass:addClass(newClassFunction)
-  table.insert(self.class,newClassFunction);
+    if renderClass == self or (not self) then
+        error("you can't use this function on none inited class, using className.new() for using this function");
+    end
+    table.insert(self.class,newClassFunction);
 end
 
 function renderClass:import(ClassName)
-	return function (prop)
-		if type(prop) == "string" then
-			local lastName = prop;
-			return function (nprop)
-				nprop.Name = lastName;
-				local item = newObject(ClassName,nprop);
-				self.storage[lastName] = item;
-				return item;
-			end;
-		end
-		return newObject(ClassName,prop);
-	end;
+    if renderClass == self or (not self) then
+        error("you can't use this function on none inited class, using className.new() for using this function");
+    end
+    return function (prop)
+        if type(prop) == "string" then
+            local lastName = prop;
+            return function (nprop)
+                nprop.Name = lastName;
+                local item = self:newObject(ClassName,nprop);
+                self.storage[lastName] = item;
+                return item;
+            end;
+        end
+        return self:newObject(ClassName,prop);
+    end;
 end
 
 function renderClass:draw(thing)
-  if type(thing) == "table" then
-    for _,nthing in pairs(thing) do
-      self:draw(thing);
+    if renderClass == self or (not self) then
+        error("you can't use this function on none inited class, using className.new() for using this function");
     end
-  else
-    if thing:IsA("ScreenGui") then
-      thing.Parent = game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui");
-    elseif self.uiHolder then
-      thing.Parent = self.uiHolder;
+    if type(thing) == "table" then
+        for _,nthing in pairs(thing) do
+            self:draw(thing);
+        end
     else
-      local drawROOT = self.drawROOT or new("ScreenGui",{
-        ResetOnSpawn = false;
-        ZIndexBehavior = Enum.ZIndexBehavior.Global;
-        Parent = game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui");
-      });
-      self.drawROOT = drawROOT;
+        if thing:IsA("ScreenGui") then
+            thing.Parent = game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui");
+        elseif self.uiHolder then
+            thing.Parent = self.uiHolder;
+        else
+            local drawROOT = self.drawROOT or self:newObject("ScreenGui",{
+                ResetOnSpawn = false;
+                ZIndexBehavior = Enum.ZIndexBehavior.Global;
+                Parent = game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui");
+            });
+            self.drawROOT = drawROOT;
+        end
     end
-  end
-  self.drawROOT = nil;
+    self.drawROOT = nil;
 end
 
 return renderClass.new(),renderClass;
